07cf988a08df6f06ed925ddbdd6ae30839a92d51
timbarretto <tim.barretto@gmail.com>
2018-02-21 13:40:20 +0000

    first commit

---
+++ Model/DataProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace TimBarretto\SampleForm\Model;
+
+/**
+ * Class DataProvider
+ */
+class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
+{
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        return [];
+    }
+}
